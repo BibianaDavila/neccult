794892e2e6ec26b60d9508cf63bbc627de21dafe
BibianaDavila <davila.bibiana@gmail.com>
2017-03-13 10:35:15 -0300

    First commit - header

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		
+		<title>NECCULT</title>
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>		
+		<script type="text/javascript" src="js/main.js"></script>		
+
+	</head>
+	<body>
+
+		<!--=== header ===-->
+		<header>
+			<div class="container">
+				<div class="row">
+
+					<!-- logo -->
+					<div class="col-md-3">
+						<img src="images/logo.png" class="logo"/>
+					</div>
+
+					<div class="col-md-9 text-right">
+						<ul class="custom-nav">
+
+							<li class="active"><a href=""><div class="square"></div>Home</a></li>
+							<li><a href=""><div class="square"></div>Sobre</a></li>
+							<li><a href=""><div class="square"></div>Metas</a></li>
+							<li><a href=""><div class="square"></div>Notícias</a></li>
+							<li><a href=""><div class="square"></div>Contato</a></li>
+
+						</ul>
+					</div>
+
+				</div>
+			</div>
+		</header>
+
+	</body>
+</html>
