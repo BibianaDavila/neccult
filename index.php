9f4fe9e15cd0696b60f5fa2aa51cb25d79e6b225
BibianaDavila <davila.bibiana@gmail.com>
2017-03-13 13:40:40 -0300

    Adicionado carousel de noticias

--- index.php
+++ index.php
@@ -10,7 +10,9 @@
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<script type="text/javascript" src="js/bootstrap.js"></script>		
-		<script type="text/javascript" src="js/main.js"></script>		
+		<script type="text/javascript" src="js/main.js"></script>	
+
+		<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">	
 
 	</head>
 	<body>
@@ -40,6 +42,195 @@
 				</div>
 			</div>
 		</header>
+
+		<!--=== conteúdo ===-->
+		<div class="content">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+
+						<!--=== carousel noticias ===-->
+						<div id="newsCarousel" class="custom-carousel carousel slide" data-ride="carousel">
+
+							<!-- indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#newsCarousel" data-slide-to="0" class="active"></li>
+								<li data-target="#newsCarousel" data-slide-to="1"></li>
+								<li data-target="#newsCarousel" data-slide-to="2"></li>
+								<li data-target="#newsCarousel" data-slide-to="3"></li>
+							</ol>
+
+							<!-- wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+	    						
+	    						<div class="item active">
+	    							<div class="carousel-inner-item">
+	    								
+	    								<!-- background -->
+	    								<div class="carousel-bg">
+	    									<div class="row">
+
+	    										<!-- image -->
+	    										<div class="col-md-7">
+	    											<div class="carousel-wrapper-img">
+	    												<div class="carousel-img">
+			    											<img src="images/teste.png">
+			    										</div>
+			    									</div>
+			    								</div>
+
+			    								<!-- texto -->
+			    								<div class="col-md-5">
+			    									<div class="carousel-title">
+			    										
+			    										CREATIVE EDGE PARTIES
+
+			    										<div class="separador"></div>
+
+			    									</div>
+			    									<div class="carousel-text">
+			    										
+			    										Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna ali
+			    									
+			    									</div>
+			    									<div class="carousel-data">
+			    										22 de Março de 2017
+			    									</div>
+			    								</div>
+			    							</div>
+										</div>									
+									</div>
+	    						</div>
+	    						<div class="item">
+	    							<div class="carousel-inner-item">
+	    								
+	    								<!-- background -->
+	    								<div class="carousel-bg">
+	    									<div class="row">
+
+	    										<!-- image -->
+	    										<div class="col-md-7">
+	    											<div class="carousel-wrapper-img">
+	    												<div class="carousel-img">
+			    											<img src="images/teste.png">
+			    										</div>
+			    									</div>
+			    								</div>
+
+			    								<!-- texto -->
+			    								<div class="col-md-5">
+			    									<div class="carousel-title">
+			    										
+			    										CREATIVE EDGE PARTIES
+
+			    										<div class="separador"></div>
+
+			    									</div>
+			    									<div class="carousel-text">
+			    										
+			    										Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna ali
+			    									
+			    									</div>
+			    									<div class="carousel-data">
+			    										22 de Março de 2017
+			    									</div>
+			    								</div>
+			    							</div>
+										</div>									
+									</div>
+	    						</div>
+	    						<div class="item">
+	    							<div class="carousel-inner-item">
+	    								
+	    								<!-- background -->
+	    								<div class="carousel-bg">
+	    									<div class="row">
+
+	    										<!-- image -->
+	    										<div class="col-md-7">
+	    											<div class="carousel-wrapper-img">
+	    												<div class="carousel-img">
+			    											<img src="images/teste.png">
+			    										</div>
+			    									</div>
+			    								</div>
+
+			    								<!-- texto -->
+			    								<div class="col-md-5">
+			    									<div class="carousel-title">
+			    										
+			    										CREATIVE EDGE PARTIES
+
+			    										<div class="separador"></div>
+
+			    									</div>
+			    									<div class="carousel-text">
+			    										
+			    										Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna ali
+			    									
+			    									</div>
+			    									<div class="carousel-data">
+			    										22 de Março de 2017
+			    									</div>
+			    								</div>
+			    							</div>
+										</div>									
+									</div>
+	    						</div>
+	    						<div class="item">
+	    							<div class="carousel-inner-item">
+	    								
+	    								<!-- background -->
+	    								<div class="carousel-bg">
+	    									<div class="row">
+
+	    										<!-- image -->
+	    										<div class="col-md-7">
+	    											<div class="carousel-wrapper-img">
+	    												<div class="carousel-img">
+			    											<img src="images/teste.png">
+			    										</div>
+			    									</div>
+			    								</div>
+
+			    								<!-- texto -->
+			    								<div class="col-md-5">
+			    									<div class="carousel-title">
+			    										
+			    										CREATIVE EDGE PARTIES
+
+			    										<div class="separador"></div>
+
+			    									</div>
+			    									<div class="carousel-text">
+			    										
+			    										Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna ali
+			    									
+			    									</div>
+			    									<div class="carousel-data">
+			    										22 de Março de 2017
+			    									</div>
+			    								</div>
+			    							</div>
+										</div>									
+									</div>
+	    						</div>
+							</div>
+
+							<!-- left and right controls -->
+							<a class="left carousel-control" href="#newsCarousel" role="button" data-slide="prev">
+								<img src="images/icons/left.png" class="arrow-icon"/>
+	 						</a>
+
+	  						<a class="right carousel-control" href="#newsCarousel" role="button" data-slide="next">
+								<img src="images/icons/right.png" class="arrow-icon"/>
+	  						</a>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
 	</body>
 </html>
