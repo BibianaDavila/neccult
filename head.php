17fbbd144e68e9e007c73c21574d68f585a082dc
BibianaDavila <davila.bibiana@gmail.com>
2017-03-27 14:44:41 -0300

    Ajustados conteúdos de head e header em arquivos separados

---
+++ head.php
@@ -0,0 +1,59 @@
+<?php
+	require_once("Includes.php");
+
+	global $idioma;
+
+    /* mudou idioma na url */
+    if (!empty($_GET["lang"])){
+        setcookie("lang", $_GET["lang"]);
+        $idioma = $_GET["lang"];
+    }
+
+    else{
+
+        /* busca idioma no cookie */
+        if (isset($_COOKIE['lang'])){
+            $idioma = $_COOKIE['lang']; 
+        }
+        else{
+            setcookie("lang", urlencode('pt'));
+            $idioma = isset($_COOKIE['lang']); 
+        }
+    } 
+
+    /* texto de acordo com o idioma */
+    switch ($idioma) {
+    	case "en":
+    		
+            $json = file_get_contents('langs/en.json');
+    		break;
+    	
+    	case "es":
+
+            $json = file_get_contents('langs/es.json');
+    		break;
+
+    	case "pt":
+    	default:    	
+         
+            $json = file_get_contents('langs/pt.json');
+    		break;
+    }
+
+    $text = json_decode($json, true);
+
+?>
+
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>		
+<script type="text/javascript" src="js/main.js"></script>	
+<script type="text/javascript" src="js/jquery.ellipsis.js"></script>
+
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">	
+
---
+++ header.php
@@ -0,0 +1,30 @@
+<!--=== header ===-->
+<header>
+	<div class="container">
+		<div class="row">
+
+			<!-- logo -->
+			<div class="col-md-3 col-sm-4 col-xs-8 col-xs-offset-2 col-sm-offset-0 text-center">
+				<a href="index.php"><img src="images/logo.png" class="logo"/></a>
+			</div>
+
+			<div class="col-md-9 col-sm-8 col-xs-12 text-right">
+				<ul class="custom-nav">
+
+					<?php
+						$url_complete = $_SERVER['REQUEST_URI'];
+						$url = explode('/', $url_complete);
+						$page = basename($url[sizeof($url)-1], '.php');
+					?>
+
+					<li class="<?php echo ($page == 'index' ? 'active' : ''); ?>"><a href="index.php"><div class="square"></div>Home</a></li>
+					<li class="<?php echo ($page == 'sobre' ? 'active' : ''); ?>"><a href="sobre.php"><div class="square"></div><?php echo $text['sobre']?></a></li>
+					<li class="<?php echo ($page == 'metas' ? 'active' : ''); ?>"><a href="metas.php"><div class="square"></div><?php echo $text['metas']?></a></li>
+					<li class="<?php echo ($page == 'noticias' ? 'active' : ''); ?>"><a href="noticias.php"><div class="square"></div><?php echo $text['noticias']?></a></li>
+					<li class="<?php echo ($page == 'contato' ? 'active' : ''); ?>"><a href="contato.php"><div class="square"></div><?php echo $text['contato']?></a></li>
+
+				</ul>
+			</div>
+		</div>
+	</div>
+</header>
