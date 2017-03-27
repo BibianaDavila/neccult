29ae5a8d92fbeca9f2bc5fdfd15488021ef456f4
BibianaDavila <davila.bibiana@gmail.com>
2017-03-27 15:12:23 -0300

    Alterado arquivo para buscar header e head do arquivo independente

--- metas.php
+++ metas.php
@@ -1,75 +1,32 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="css/main.css">
 		
-		<title>NECCULT</title>
+		<?php include_once("head.php"); ?>
+		
+		<title><?echo $text['metas'];?> | NECCULT</title>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.js"></script>		
-		<script type="text/javascript" src="js/main.js"></script>	
-
-		<link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">	
-
-	</head>
+	</head>	
 	<body>
 
 		<!--=== header ===-->
-		<header>
-			<div class="container">
-				<div class="row">
-
-					<!-- logo -->
-					<div class="col-md-3">
-						<img src="images/logo.png" class="logo"/>
-					</div>
-
-					<div class="col-md-9 text-right">
-						<ul class="custom-nav">
-
-							<li><a href="index.php"><div class="square"></div>Home</a></li>
-							<li><a href="sobre.php"><div class="square"></div>Sobre</a></li>
-							<li class="active"><div class="square"></div>Metas</li>
-							<li><a href=""><div class="square"></div>Notícias</a></li>
-							<li><a href="contato.php"><div class="square"></div>Contato</a></li>
-
-						</ul>
-					</div>
-
-				</div>
-			</div>
-		</header>
+		<?php
+			include_once("header.php");
+		?>
 
 		<!--=== conteúdo ===-->
 		<div class="content">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="col-md-8 col-md-offset-2 col-xs-12 metas-container text-center">
 
 						<div class="separador-vertical"></div>
 					
 						<div class="metas-text">
 
-							<p>O NECCULT atuará considerando quatro conjuntos de diretrizes relacionadas ao ensino, à pesquisa e à extensão: consolidação institucional, produção de conhecimento acadêmico, interação com a sociedade para a inovação e internacionalização. O foco principal permanece na pesquisa, mas, fundamentalmente, enquanto vetor para inovação social baseada no papel transformador da cultura e da criatividade.</p>
-
-							<br/>
-
-							<p><b>DIRETRIZ 1 -</b> Consolidação institucional: concluir o processo de institucionalização do NECCULT na UFRGS no prazo de três anos;</p>
-
-							<br/>
-
-							<p><b>DIRETRIZ 2 -</b> Produção de conhecimento acadêmico: consolidar a reputação científica e institucional do NECCULT junto aos interlocutores da administração pública federal, estadual e municipal, bem como junto à sociedade civil e à comunidade universitária;</p>
-
-							<br/>
-
-							<p><b>DIRETRIZ 3 -</b> Interação com a sociedade para a inovação: construir iniciativas – em parceria com a sociedade civil e os diferentes órgãos da Administração Pública – voltadas ao impulso da cultura e da criatividade na forma de ensino, pesquisa e extensão;</p>
-
-							<br/>
-
-							<p><b>DIRETRIZ 4 -</b> Internacionalização: consolidar o processo de internacionalização do NECCULT, conforme diretrizes da UFRGS.</p>
+							<?php foreach($text['paginaMetas-texto'] as $key => $value):?>
+								<p><?php echo $value['paragrafo']; ?></p><br/>
+							<?php endforeach;?>
 
 						</div>
 
@@ -77,6 +34,17 @@
 
 					</div>
 				</div>
+			</div>
+		</div>
+
+		<!-- idioma -->
+		<div class="idioma-content">
+			<div class="container text-center">
+						
+				<a href="metas.php?lang=pt" class="button">PT</a>
+				<a href="metas.php?lang=es" class="button">ES</a>
+				<a href="metas.php?lang=en" class="button">EN</a>
+
 			</div>
 		</div>
 	</body>
